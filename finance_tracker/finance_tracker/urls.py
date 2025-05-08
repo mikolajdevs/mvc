865facd7f2a2bdd8ef109125c0cc3c65d34b5fef
mikolajdevs <mikolajdevs@gmail.com>
2025-05-08 21:19:36 +0200

    feature: init transactions view, controller (project)

--- finance_tracker/finance_tracker/urls.py
+++ finance_tracker/finance_tracker/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import RedirectView
+from django.urls import include, path
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/transactions/', permanent=False)),
+    path('transactions/', include("transactions.urls")),
     path('admin/', admin.site.urls),
 ]
